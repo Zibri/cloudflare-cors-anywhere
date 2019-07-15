3bbdb4ad78435460c557e4f67b63ec4ad4e7520a
Zibri <Zibri@users.noreply.github.com>
2019-07-15 16:48:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+whitelist = [ "zibri.org$", "test\\..*" ];  // regexp for whitelisted urls
+
+function isWhitelisted(uri) {
+    var ret=false;
+    if (typeof uri == "string") {
+        whitelist.forEach((m)=>{
+	          if (uri.match(m)!=null) ret=true;
+        });
+    } else {         // decide what to do when Origin is null
+    	  ret=true;    // true accepts null origins false rejects them.
+    }
+    return ret;
+}
+
+
 addEventListener("fetch", async event=>{
     event.respondWith((async function() {
         var origin_url = new URL(event.request.url);
 
-        if (origin_url.search.startsWith("?")) {
-            var fetch_url = unescape(origin_url.search.substr(1));
-            var response = await fetch(fetch_url,event.request);
-            var myHeaders = new Headers(response.headers);
+        function fix(myHeaders) {
             myHeaders.set("Access-Control-Allow-Origin", "*");
             acrh = event.request.headers.get("access-control-request-headers");
 
@@ -15,52 +27,57 @@ addEventListener("fetch", async event=>{
             }
 
             myHeaders.delete("X-Content-Type-Options");
-            var body = await response.arrayBuffer();
-            var init = {
-                headers: myHeaders
-            };
-            return new Response(body,init);
-        } else {
-            var myHeaders = new Headers();
-            myHeaders.set("Access-Control-Allow-Origin", "*");
-            acrh = event.request.headers.get("access-control-request-headers");
-
-            if (acrh) {
-                myHeaders.set("Access-Control-Allow-Headers", acrh);
-                myHeaders.set("Access-Control-Allow-Credentials", "true");
-            }
-
-            myHeaders.delete("X-Content-Type-Options");
-
-            if (typeof event.request.cf != "undefined") {
-                if (typeof event.request.cf.country != "undefined") {
-                    country = event.request.cf.country;
-                } else
-                    country = false;
-
-                if (typeof event.request.cf.colo != "undefined") {
-                    colo = event.request.cf.colo;
-                } else
-                    colo = false;
-            } else {
-                country = false;
-                colo = false;
-            }
-
-            orig = event.request.headers.get("Origin");
-            return new Response(
-                "CF-CORS-ANYWHERE\n\n"
-                + "Usage\n" 
-                + origin_url.origin + "/?uri\n\n" 
-                + "Limits: 100,000 requests/day\n" 
-                + "          1,000 requests/10 minutes\n\n" 
-                + (orig != null ? "Origin: " + orig + "\n" : "") 
-                + "Ip: " + event.request.headers.get("CF-Connecting-IP") + "\n" 
-                + (country ? "Country: " + country + "\n" : "") 
-                + (colo ? "Datacenter: " + colo + "\n" : "") 
-                + "\n",{status: 200, headers: myHeaders}
-                );
+            return myHeaders;
         }
+        var fetch_url = unescape(origin_url.search.substr(1));
+        var orig = event.request.headers.get("Origin");
+        if (isWhitelisted(orig)) {
+            if (origin_url.search.startsWith("?")) {
+                var response = await fetch(fetch_url,event.request);
+                var myHeaders = new Headers(response.headers);
+                myHeaders = fix(myHeaders);
+
+                var body = await response.arrayBuffer();
+                var init = {
+                    headers: myHeaders
+                };
+                return new Response(body,init);
+
+            } else {
+                var myHeaders = new Headers();
+                myHeaders = fix(myHeaders);
+
+                if (typeof event.request.cf != "undefined") {
+                    if (typeof event.request.cf.country != "undefined") {
+                        country = event.request.cf.country;
+                    } else
+                        country = false;
+
+                    if (typeof event.request.cf.colo != "undefined") {
+                        colo = event.request.cf.colo;
+                    } else
+                        colo = false;
+                } else {
+                    country = false;
+                    colo = false;
+                }
+
+                return new Response(
+                    "CF-CORS-ANYWHERE\n\n"
+                    + "Usage\n" 
+                    + origin_url.origin + "/?uri\n\n" 
+                    + "Limits: 100,000 requests/day\n" 
+                    + "          1,000 requests/10 minutes\n\n" 
+                    + (orig != null ? "Origin: " + orig + "\n" : "") 
+                    + "Ip: " + event.request.headers.get("CF-Connecting-IP") + "\n" 
+                    + (country ? "Country: " + country + "\n" : "") 
+                    + (colo ? "Datacenter: " + colo + "\n" : "") 
+                    + "\n",{status: 200, headers: myHeaders}
+                    );
+            }
+          } else {
+          	return new Response('403 Forbidden',{status: 403});
+          }
     }
     )());
 });
